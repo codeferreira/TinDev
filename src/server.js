61a08f2ed3a695874e7466b479e083c1eb7fc07c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-08 23:18:02 -0300

    Express Server Config

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb://omnistack:123#mudar@ds261277.mlab.com:61277/omnistack08', {
+  useNewUrlParser: true
+})
+
+server.use(express.json())
+server.use(routes);
+
+server.listen(3333);
