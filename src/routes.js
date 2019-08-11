6818a5b387174200bdeed7b7780983aadbbeccb9
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-11 19:40:33 -0300

    Routes
    GET /developers - All Developers not Liked or Disliked by the logged user
    
    POST /developers - Create a new user in DB
    
    POST /developers/:developerId/likes - Like other user
    
    POST /developers/:developerId/dislikes - Dislike other user

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const DevController = require('./controllers/Dev')
+const DeveloperController = require('./controllers/Developer');
+const LikeController = require('./controllers/Like');
+const DislikeController = require('./controllers/Dislike');
 
 const routes = express.Router();
 
-routes.post('/devs', DevController.store)
+routes.get('/developers', DeveloperController.index);
+routes.post('/developers', DeveloperController.store);
+routes.post('/developers/:developerId/likes', LikeController.store);
+routes.post('/developers/:developerId/dislikes', DislikeController.store);
 
 module.exports = routes;
