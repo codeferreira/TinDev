ecb7872e3bfab6bf9d15bb2ec56a89a85dd8599b
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-08-11 19:40:48 -0300

    CORS added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -7,9 +8,10 @@ const server = express();
 
 mongoose.connect('mongodb://omnistack:123#mudar@ds261277.mlab.com:61277/omnistack08', {
   useNewUrlParser: true
-})
+});
 
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
